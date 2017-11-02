5fb9be127cf38f3dcae1b0ee8aa940b38e0ce14a
rashwanov <rashwanov@gmail.com>
2017-11-02 17:57:29 +0200

    Updated
    Added Headers so that request is not blocked.
    Added Set() function to remove duplicated results before displaying
    Added Function to handle "No Email Found" errors

--- urllist.py
+++ urllist.py
@@ -1,19 +1,24 @@
-import os
-from bs4 import BeautifulSoup as soup
+import urllib2
 import re
-from urllib2 import urlopen
 
 def scrape(_url):
-	f = urlopen(_url)
-	s = soup(f, "html.parser")
-	s = s.get_text()
-	emails = re.findall(r"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,3}",s)
-	if len(emails) == 0:
-		print("Sorry, no emails found")
+
+	user_agent = 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_4; en-US) AppleWebKit/534.3 (KHTML, like Gecko) Chrome/6.0.472.63 Safari/534.3'
+	headers = { 'User-Agent' : user_agent }
+	req = urllib2.Request(_url, None, headers)
+	response = urllib2.urlopen(req)
+	page = response.read()
+	emails = re.findall(r"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,3}",page)
+	_noDups = list(set(emails))
+
+	if len(_noDups) == 0:
+		noEmailFound(_url)
 	else:
-		count = 1
-		for item in emails:
-			print(item)
+
+		for line in _noDups:
+			print line.strip()
+
+	response.close()
 
 
 def urlHandler(file1):
@@ -29,9 +34,13 @@ def getFile():
 	urlHandler(_fileName)
 	return _fileName
 
+def noEmailFound(_url):
+	print ("-------------------------------------")
+	print ("Sorry no emails found on url: " + _url)
+	print ("-------------------------------------")
+
 
 def main():	
 	getFile()
 
 main()
-   
